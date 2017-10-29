c87db4f1094da8368721c9a761ea0b703f3f1ac0
adhamGatriff <adhamgatriff@gmail.com>
2017-10-28 20:40:36 -0400

    nuevas letras y eso

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,13 @@
 @section('title','principal')
 
 @section('main')
+<a class="btn-floating btn-large waves-effect waves-light degradado"><i class="material-icons">add</i></a>
+<a class="waves-effect waves-light btn degradado Wradius"><i class="material-icons left">cloud</i>button</a>
+<a class="waves-effect waves-moradito btn-flat">Button</a>
+
+<a class="waves-effect moradito btn-flat ">Button</a>
+
+<h1 class="opensans">prueba de TITULO</h1>
 <div class="flow-text">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
